5e906c1988a50281b4a06d336fbe450630a8a90e
zhy <1817705128@qq.com>
2021-02-20 17:04:00 +0800

    gunicorn.conf修改

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '=udls*&3#q**0bm!o#l2s=bu3tp&e-4yv&=-a$u2$#w-o6f*wt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','0.0.0.0']
 
 # Application definition
 SITE_ID = 1
